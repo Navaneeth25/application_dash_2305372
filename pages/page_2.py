aa85a91979d3ceb94a63787ae5058fdb58d513a5
Navaneeth25 <79408053+Navaneeth25@users.noreply.github.com>
2023-09-13 23:52:23 +0100

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -13,8 +13,8 @@ warnings.filterwarnings('ignore')
 import dash_bootstrap_components as dbc
 dash.register_page(__name__, name='Animated plots')
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
-covid_dataset=pd.read_csv('https://raw.githubusercontent.com/Navaneeth25/covid_dataset/main/OxCGRT_summary20200520.csv')
-country_continent_dataset=pd.read_csv('https://raw.githubusercontent.com/Navaneeth25/covid_dataset/main/country-and-continent.csv')
+covid_dataset=pd.read_csv('C:/Users/navan/Downloads/OxCGRT_summary20200520.csv')
+country_continent_dataset=pd.read_csv('C:/Users/navan/Downloads/country-and-continent.csv')
 country_continent_dataset.dropna(inplace=True)
 merged_dataset= covid_dataset.merge(country_continent_dataset, how = 'left', on = 'CountryCode')
 null_continents=merged_dataset[merged_dataset['Continent_Name'].isna()]
@@ -25,11 +25,10 @@ merged_dataset = merged_dataset.groupby('CountryName').apply(lambda x: x.sort_va
 fillna_values = merged_dataset.groupby(['CountryName']).fillna(method='ffill').fillna(method='bfill')
 fillna_values['CountryName']=merged_dataset['CountryName']
 fillna_values = fillna_values.groupby('CountryName').apply(lambda x: x.sort_values('Date')).reset_index(drop=True)
-data=fillna_values.query('Date==20200520').drop_duplicates(['CountryCode']).reset_index(drop=True)
+fillna_values['Date'] = pd.to_datetime(fillna_values['Date'], format='%Y%m%d').dt.date
 df3 = fillna_values.query("CountryName == ['United States','Russia','Brazil','United Kingdom','Spain']")
 df3.reset_index(drop=True,inplace=True)
 df3=df3.drop_duplicates(['Date','CountryName']).reset_index(drop=True)
-df3['Date'] = pd.to_datetime(df3['Date'], format='%Y%m%d')
 df3['School closing'] = df3['School closing'].map({0: 'no measures', 1: 'recommend closing',2:'require localised closing',3:'require all closing'})
 df3['Stay at home requirements'] = df3['Stay at home requirements'].map({0: 'no measures', 1: 'recommend not leaving house',2:'only some exceptions',3:'minimal exceptions'})
 fillna_values = fillna_values.drop_duplicates(  subset = ['CountryName', 'Date'],
@@ -39,11 +38,11 @@ df4=fillna_values.copy()
 df4= fillna_values[fillna_values['ConfirmedDeaths'] != 0]
 df4['date'] = pd.to_datetime(df4['Date'], format='%Y%m%d')
 data=fillna_values.copy()
-fillna_values['date'] = pd.to_datetime(fillna_values['Date'], format='%Y%m%d')
-
 fig6= barplot(data,  item_column='CountryName', value_column='ConfirmedCases', time_column='Date')
 fig6.plot(item_label = 'Top 10 countries', value_label = 'cases', frame_duration = 800)
 fig6=fig6.fig
+fig6.update_layout(title_text= "Race bar plot for top 10 countries",title_x=0.3,title_font_family="Sitka Small",
+    title_font_color="green")
 sidebar = html.Div(
     [
         dbc.Nav(
@@ -70,8 +69,12 @@ def updatefig(g):
         fig6= barplot(data,  item_column='CountryName', value_column='ConfirmedCases', time_column='Date')
         fig6.plot(item_label = 'Top 10 countries', value_label = 'cases', frame_duration = 800)
         fig6=fig6.fig
+        fig6.update_layout(title_text= "Race bar plot for top 10 countries",title_x=0.3,title_font_family="Sitka Small",
+    title_font_color="green")
         return fig6
     else:
         fig6 = px.scatter_geo(fillna_values, locations="CountryCode", color="Continent_Name",hover_name="CountryName", 
                     size="ConfirmedCases",animation_frame="Date",size_max=20,projection="natural earth")
+        fig6.update_layout(title_text= "Scatter geo of world 03/2020 - 05/2020",title_x=0.3,title_font_family="Sitka Small",
+    title_font_color="green")
         return fig6
